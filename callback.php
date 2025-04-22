c3b8ce4093938df4a134ecef32fa089e7aad6704
omarabdalla1 <47723175+omarabdalla1@users.noreply.github.com>
2025-04-22 15:22:36 +0300

    Update callback.php

--- callback.php
+++ callback.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_GET['code'])) {
     $client_id = "0c264d61-51ab-4281-b9e4-207c98f42ed1";
     $client_secret = "a23a1c840b965e078168823486da1650";
-    $redirect_uri = "http://localhost/salla_api_project/callback.php";
+    $redirect_uri = "https://tawasul-bot.onrender.com//callback.php";
     $code = $_GET['code'];
     
     $url = "https://auth.salla.sa/oauth/token";
